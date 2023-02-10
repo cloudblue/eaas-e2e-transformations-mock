27bfea3bec5941a6174b02efa712cbd7bb6ed90b
Francesco Faraone <francesco.faraone@cloudblue.com>
2023-02-10 08:14:45 +0100

    Fix transformations app

--- connect_ext/transformations.py
+++ connect_ext/transformations.py
@@ -1,34 +1,21 @@
 from connect.eaas.core.decorators import transformation
-from connect.eaas.core.extension import TransformationBase
+from connect.eaas.core.extension import TransformationsApplicationBase
 
 
-@transformation(
-    name='Multiplier transformation',
-    description='The transformation function that multiplies selected columns',
-    edit_dialog_ui='/static/transformations/multiplier_settings.html',
-)
-class MultiplierTransformation(TransformationBase):
-    multiplier = 1.25
+class E2ETransformationsApplication(TransformationsApplicationBase):
 
-    def process_row(self, row):
-        row.value = row.value * self.multiplier
-
+    @transformation(
+        name='Multiplier transformation',
+        description='The transformation function that multiplies selected columns',
+        edit_dialog_ui='/static/transformations/multiplier_settings.html',
+    )
+    def transform_1(self, row):
         return row
 
-    def initialize(self, input_columns, stream):
-        self.input_columns = input_columns
-        self.stream = stream
-
-
-@transformation(
-    name='Fake transformation',
-    description='The transformation function that do nothihg',
-    edit_dialog_ui='/static/transformations/simple_settings.html',
-)
-class SecondTransformation(TransformationBase):
-
-    def process_row(self, row):
-        pass
-
-    def initialize(self):
-        pass
+    @transformation(
+        name='Fake transformation',
+        description='The transformation function that do nothihg',
+        edit_dialog_ui='/static/transformations/simple_settings.html',
+    )
+    def transform_2(self, row):
+        return row
